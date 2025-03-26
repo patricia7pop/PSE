2ca354b3a5a5aa54f12e19a8dba2aa2a0389fcea
Dragan Andrei-Valentin <aney2000@yahoo.com>
2025-03-26 18:36:09 +0200

    Modified Dragan's migrations

--- migrations/Version20250319163258.php
+++ migrations/Version20250319163258.php
@@ -21,13 +21,13 @@ final class Version20250319163258 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE dragan_page_content (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content CLOB NOT NULL)');
-        $this->addSql('DROP TABLE page_content');
+        $this->addSql('DROP TABLE IF EXISTS page_content');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE page_content (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL COLLATE "BINARY", content CLOB NOT NULL COLLATE "BINARY")');
-        $this->addSql('DROP TABLE dragan_page_content');
+        $this->addSql('DROP TABLE IF EXISTS dragan_page_content');
     }
 }
