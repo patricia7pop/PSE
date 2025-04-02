927822eb2e5c78f87dc07bbb5725cf3c06fdc9b2
Truta George-Marius <100347907+MrMustacheXD@users.noreply.github.com>
2025-04-02 15:35:47 +0300

    Update TrutaGeorgeMariusController.php

--- src/Controller/TrutaGeorgeMariusController.php
+++ src/Controller/TrutaGeorgeMariusController.php
@@ -10,6 +10,6 @@ class TrutaGeorgeMariusController extends AbstractController
     #[Route('/Truta-George-Marius', name: 'Truta_George_Marius')]
     public function index(): Response
     {
-        return $this->render('Truta_George_Marius/index.html.twig');  // Schimbă 'ion_popescu' cu numele tău
+        return $this->render('Truta_George_Marius/index.html.twig'); 
     }
 }
